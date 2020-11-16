ab8ec9687e0b6b2b0476e470f3c0a3bb11ef7a29
Giedrius <slikas.giedrius@gmail.com>
2020-11-16 23:50:14 +0200

    Simplify testing

--- app/src/androidTest/java/com/giedrius/iptv/MainTest.kt
+++ app/src/androidTest/java/com/giedrius/iptv/MainTest.kt
@@ -1,29 +1,38 @@
 package com.giedrius.iptv
 
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.fragment.app.viewModels
-import com.giedrius.iptv.ui.main.MainFragment
+import com.giedrius.iptv.data.model.Data
+import com.giedrius.iptv.data.repository.MainRepository
 import com.giedrius.iptv.ui.main.MainViewModel
-import com.giedrius.iptv.utils.launchFragmentInHiltContainer
+import com.google.firebase.database.DatabaseReference
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import okhttp3.Dispatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import retrofit2.Response
 
 @HiltAndroidTest
 class MainTest {
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
+    var mainRepository: MainRepository = mockk(relaxed = true)
+    var firebaseDatabase: DatabaseReference = mockk(relaxed = true)
+    lateinit var viewModel: MainViewModel
 
     @Before
-    fun init() {
-        hiltRule.inject()
+    fun setUp() {
+        viewModel = MainViewModel(mainRepository, firebaseDatabase)
     }
 
     @Test
     fun mainFragment() {
-        val scenario = launchFragmentInHiltContainer<MainFragment> {}
-        
+        //mock coroutine
+//        runBlocking {
+//            mainRepository.getData() returns Response.success(Data(id = 1, value = "Test"))
+//        }
+
     }
 }
