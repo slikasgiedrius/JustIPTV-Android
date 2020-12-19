56890832d8ff6fb6a25386f389c0417832e2307a
Giedrius Slikas <GSLI@danskebank.lt>
2020-12-19 17:22:00 +0200

    Add a big part of compose

--- app/src/main/java/com/giedrius/iptv/ui/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/giedrius/iptv/ui/introduction/IntroductionActivity.kt
@@ -2,7 +2,14 @@ package com.giedrius.iptv.ui.introduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
 import com.giedrius.iptv.R
+import com.giedrius.iptv.compose.ComposeTestTheme
 
 class IntroductionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
