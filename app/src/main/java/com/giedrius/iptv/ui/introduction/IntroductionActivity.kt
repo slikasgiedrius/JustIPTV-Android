e7b3e38e69b9a739fbabdb01e10184585e5d8ef0
Giedrius Slikas <GSLI@danskebank.lt>
2021-01-08 00:40:51 +0200

    In progress with colors

--- app/src/main/java/com/giedrius/iptv/ui/introduction/IntroductionActivity.kt
+++ app/src/main/java/com/giedrius/iptv/ui/introduction/IntroductionActivity.kt
@@ -1,13 +1,15 @@
 package com.giedrius.iptv.ui.introduction
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.giedrius.iptv.ui.MainActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.giedrius.iptv.R
 import com.giedrius.iptv.databinding.ActivityIntroductionBinding
+import com.giedrius.iptv.ui.MainActivity
+import com.shuhart.stepview.StepView
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class IntroductionActivity : AppCompatActivity(R.layout.activity_introduction) {
@@ -23,28 +25,25 @@ class IntroductionActivity : AppCompatActivity(R.layout.activity_introduction) {
         setContentView(view)
 
         handleObservers()
+        styleStepView()
     }
 
     private fun handleObservers() {
         viewModel.downloadRepository.onDataDownloaded.observe(this) {
-            binding.sDownloadContent.isChecked = it
+            binding.stepView.go(1, true)
         }
         viewModel.downloadRepository.onFilePatchChanged.observe(this) {
             viewModel.loadChannels(it)
         }
         viewModel.onExtractionCompleted.observe(this) {
-            binding.sExtractItems.isChecked = it
+            binding.stepView.go(2, true)
         }
         viewModel.onSavingCompleted.observe(this) {
-            binding.sSaveItems.isChecked = true
+            binding.stepView.go(3, true)
         }
         viewModel.channelsRepository.savedChannels.observe(this) {
             if (it.count() != 0) {
-                binding.sDownloadContent.isChecked = true
-                binding.sExtractItems.isChecked = true
-                binding.sSaveItems.isChecked = true
-                binding.sRetrieveItems.isChecked = true
-
+                binding.stepView.go(4, true)
                 navigateToMainActivity()
             }
         }
@@ -54,5 +53,18 @@ class IntroductionActivity : AppCompatActivity(R.layout.activity_introduction) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun styleStepView() {
+        binding.stepView.state
+            .steps(listOf(
+                "Download content from URL",
+                "Extract items from content",
+                "Save items to local storage",
+                "Retrieve items from local storage")
+            )
+            .animationType(StepView.ANIMATION_ALL)
+            .animationDuration(resources.getInteger(android.R.integer.config_mediumAnimTime))
+            .commit()
     }
 }
--- app/src/main/java/com/giedrius/iptv/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/giedrius/iptv/ui/settings/SettingsFragment.kt
@@ -1,9 +1,11 @@
 package com.giedrius.iptv.ui.settings
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
@@ -45,10 +47,24 @@ class SettingsFragment : Fragment(R.layout.settings_fragment) {
         binding.iwDeleteAllChannels.setOnClickListener { viewModel.deleteAllChannels() }
         binding.iwDeleteAllFavouriteChannels.setOnClickListener { viewModel.deleteAllFavouriteChannels() }
         binding.bComposeScreen.setOnClickListener { startComposeActivity() }
+        binding.bIntroduction.setOnClickListener { startIntroductionActivity() }
+        binding.bTheme.setOnClickListener {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Configuration.UI_MODE_NIGHT_NO ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
     }
 
     private fun startComposeActivity() {
         val action = SettingsFragmentDirections.actionSettingsFragmentToComposeActivity()
+        view?.findNavController()?.navigate(action)
+    }
+
+    private fun startIntroductionActivity() {
+        val action = SettingsFragmentDirections.actionSettingsFragmentToIntroductionActivity()
         view?.findNavController()?.navigate(action)
     }
 }
